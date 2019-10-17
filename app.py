2643950257e33ea1f40b5e3bb9089774a308312c
manuelguido <manuelguido.m@gmail.com>
2019-10-17 16:27:26 -0300

    initial templates + proyect setup

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, escape, request
+from flask import render_template, g, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('home.html')
+
+@app.route('/panel')
+def panel():
+    return render_template('panel.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('home_login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) 
--- config/development.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = "dev"
-DEBUG = True
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASS = ''
-DB_NAME = 'proyecto'
