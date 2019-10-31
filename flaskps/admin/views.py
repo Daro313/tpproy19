da83055be0a55109fc135e6443f3f6ab425cdc52
Mariano Otero <mariano.otero@moni.com.ar>
2019-10-30 23:52:14 -0300

    move logit to user app

--- flaskps/admin/views.py
+++ flaskps/admin/views.py
@@ -8,52 +8,8 @@ from flaskps.models import User
 from . import admin
 
 
-@admin.route('/user_create_form')
-# @login_required descomentar despues de crear el primer usuario
-def user_create_form():
-    """
-    Muestra el template de creacion de usuario
-    """
-    return render_template('admin/create_user.html')
 
 
-@admin.route('/create_user', methods=['POST'])
-def create_user():
-    """
-    Si los datos son validos crea un nuevo usuario
-    """
-    data_dict = {}
-    data = request.data.decode()
-    data = data.replace("{", "").replace("}", "").replace('\"', "")
-    data = data.split(',')
-
-    for d in data:
-        d = d.split(':')
-        data_dict.update({d[0]: d[1]})
-
-    name = data_dict.get('name')
-    surname = data_dict.get('surname')
-    email = data_dict.get('email')
-    password = data_dict.get('password')
-    username = data_dict.get('username')
-    rol = data_dict.get('rol')
-
-    user = User(
-        first_name=name,
-        last_name=surname,
-        email=email,
-        username=username,
-        rol=rol
-    )
-    user.password = password
-    db.session.add(user)
-    try:
-        db.session.commit()
-    except:
-        db.session.rollback()
-        return render_template('admin/create_user.html'), 403
-
-    return render_template('admin/create_user.html'), 201, {'msg': 'el usuario se creo con exito'}
 
 
 
--- flaskps/users/views.py
+++ flaskps/users/views.py
@@ -9,6 +9,53 @@ from . import users
 def update_user_template():
     pass
 
+@users.route('user/create_form')
+# @login_required descomentar despues de crear el primer usuario
+def user_create_form():
+    """
+    Muestra el template de creacion de usuario
+    """
+    return render_template('user/create_user.html')
+
+
+@users.route('user/create', methods=['POST'])
+def create():
+    """
+    Si los datos son validos crea un nuevo usuario
+    """
+    data_dict = {}
+    data = request.data.decode()
+    data = data.replace("{", "").replace("}", "").replace('\"', "")
+    data = data.split(',')
+
+    for d in data:
+        d = d.split(':')
+        data_dict.update({d[0]: d[1]})
+
+    name = data_dict.get('name')
+    surname = data_dict.get('surname')
+    email = data_dict.get('email')
+    password = data_dict.get('password')
+    username = data_dict.get('username')
+    rol = data_dict.get('rol')
+
+    user = User(
+        first_name=name,
+        last_name=surname,
+        email=email,
+        username=username,
+        rol=rol
+    )
+    user.password = password
+    db.session.add(user)
+    try:
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return render_template('user/create_user.html'), 403
+
+    return render_template('user/create_user.html'), 201, {'msg': 'el usuario se creo con exito'}
+
 
 @users.route('/user/update/<user_id>', methods=['POST'])
 def update(user_id):
@@ -36,22 +83,25 @@ def update(user_id):
     return redirect('/user/list')
 
 
-
 @users.route('/user/list', methods=['GET'])
 def list():
     users = User.query.all()
     return render_template('users/list.html', user_list=users)
+
 
 @users.route('/user/detail/<username>')
 def detail(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('users/detail.html', user=user)
 
+
 def delete_user():
     pass
 
+
 def activate_user():
     pass
+
 
 def deactivate_user():
     pass
