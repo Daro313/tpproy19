9bbc50d752625740e0bc4cc196700c5acffedcc5
Mariano Otero <mariano.otero@moni.com.ar>
2019-11-20 22:18:40 -0300

    modifica clave prod

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:admin@localhost/grupo8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://grupo8:MTFhMWVmMDkxYmE2@https://sql.proyecto2019.linti.unlp.edu.ar/grupo8'
