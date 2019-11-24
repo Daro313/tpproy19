db6b25cd8e8757e579dd9daa3b50abfe92534089
marian <mariano.m3n@gmail.com>
2019-11-24 11:15:40 -0300

    agrega key y sql url dentro de la clase production

--- config.py
+++ config.py
@@ -21,6 +21,8 @@ class ProductionConfig(Config):
     """
 
     DEBUG = False
+    SECRET_KEY = 'p9Bv<3Eid9%$i01'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://grupo8:MTFhMWVmMDkxYmE2@https://sql.proyecto2019.linti.unlp.edu.ar/grupo8'
 
 
 app_config = {
