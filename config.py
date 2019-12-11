de7a72798228349c414906a3fa542739905532ab
Dario D'Angelo <darodangelo@gmail.com>
2019-12-11 20:05:19 -0300

    Se cambian configuraciones para correr en el servidor

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class ProductionConfig(Config):
 
     DEBUG = False
     SECRET_KEY = 'p9Bv<3Eid9%$i01'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://grupo8:MTFhMWVmMDkxYmE2@https://sql.proyecto2019.linti.unlp.edu.ar/grupo8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://grupo8:MTFhMWVmMDkxYmE2@127.0.0.1/grupo8'
 
 
 app_config = {
