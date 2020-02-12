8b412ef7d88b2b54fb777015406429ac54317ee9
Dario D'Angelo <darodangelo@gmail.com>
2020-02-11 23:52:00 -0300

    agrega barrios, escuelas, niveles a models

--- flaskps/app/students/forms.py
+++ flaskps/app/students/forms.py
@@ -5,14 +5,13 @@ from wtforms import (
     PasswordField,
     validators,
     DateField,
-    SelectField
+    SelectField,
+    IntegerField
 )
 
 
 from .constants import (
     GENDER_CHOICES,
-    LEVEL_CHOICES,
-    NEIGHBORHOOD_CHOICES,
 )
 
 class CreateStudentsForm(Form):
@@ -22,11 +21,12 @@ class CreateStudentsForm(Form):
     borned = StringField('Lugar de nacimiento')
     locality = StringField('Localidad', [validators.DataRequired(message='Este es un campo requerido')])
     address = StringField('Direccion', [validators.DataRequired(message='Este es un campo requerido')])
-    neighborhood = SelectField('Barrio', [validators.DataRequired(message='Este es un campo requerido')], choices=NEIGHBORHOOD_CHOICES)
+    neighborhood = StringField('Barrio', [validators.DataRequired(message='Este es un campo requerido')])
     gender = SelectField('Genero', [validators.DataRequired(message='Este es un campo requerido')], choices=GENDER_CHOICES)
     document_type = StringField('Tipo de documento', [validators.DataRequired(message='Este es un campo requerido')])
     document_number = StringField('Numero de documento', [validators.DataRequired(message='Este es un campo requerido')])
     tutor = StringField('Tutor', [validators.DataRequired(message='Este es un campo requerido')])
     phone = IntegerField('Telefono', [validators.DataRequired(message='Solo se permite ingresar numeros')])
     school = StringField('Escuela', [validators.DataRequired(message='Este es un campo requerido')])
-    level = SelectField('Nivel', [validators.DataRequired(message='Este es un campo requerido')], choices=LEVEL_CHOICES)
+    level = StringField('Nivel', [validators.DataRequired(message='Este es un campo requerido')])
+    tutor_name = StringField('apellido', [validators.DataRequired(message='Este es un campo requerido')])
